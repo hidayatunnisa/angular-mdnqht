a1d8624e6ab5ad45f6f58af296be2d85951d1243
hidayatunnisa <shaik.hidayat@praxislogica.com>
2020-05-28 17:15:42 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 // import { HeroserviceService } from './heroservice.service';
 import { MessagesComponent } from './messages/messages.component';
+import { RoutingModule } from './routing/routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent,MessagesComponent],
+  imports:      [ BrowserModule, FormsModule, RoutingModule ],
+  declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent,MessagesComponent, DashboardComponent],
   bootstrap:    [ AppComponent ],
   providers: []//HeroserviceService
 })
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HerosComponent} from './heroes/heroes.component';
-import { RouterModule } from '@angular/router';
-const routes:Routes=[
-  {path:'heores',component:HerosComponent}
+import { HeroesComponent } from '../heroes/heroes.component';
+import { RouterModule, Routes } from '@angular/router';
+const routes:Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  {path:'',redirectTo:'heroes', pathMatch:'full'}
 ];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
   ],
-  exports:[RouterModule],
+  exports: [RouterModule],
   declarations: []
 })
 export class RoutingModule { }
